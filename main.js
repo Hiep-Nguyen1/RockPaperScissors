2931f0ab3a698303c7ef2c50c362e48d246c6f1e
Hiep Nguyen <122915998+Hiep-Nguyen1@users.noreply.github.com>
2023-05-02 15:47:29 -0500

    add win/lose logic to game rounds

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
 function round(playerChoice,compChoice){
-
+    playerChoice = convertToNum(playerChoice);
+    if (playerChoice == 0 && compChoice == 2){
+        console.log("You win because " + convertToWord(playerChoice) + 
+                            " beats " + convertToWord(compChoice) + ".");
+    }
+    else if (playerChoice==compChoice){
+        console.log("It's a tie.");
+    }
+    else if (playerChoice>compChoice){
+        console.log("You win because " + convertToWord(playerChoice) + 
+                            " beats " + convertToWord(compChoice) + ".");
+    }
+    else{
+        console.log("You lose because " + convertToWord(compChoice) + 
+        " beats " + convertToWord(playerChoice) + ".");
+    }
 }
 
 function convertToNum(choice){
@@ -10,6 +25,19 @@ function convertToNum(choice){
             return 1;
         case "scissor":
             return 2;
+        default:
+            console.log("Error, choice could not be converted");
+    }
+}
+
+function convertToWord(choice){
+    switch(choice){
+        case 0:
+            return "rock";
+        case 1:
+            return "paper";
+        case 2:
+            return "scissor";
         default:
             console.log("Error, choice could not be converted");
     }
